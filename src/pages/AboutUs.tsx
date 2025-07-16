24674ebcd0d8b42e0694d3c1444a0ec06152f2e9
RahulAdminRecruitIQ <rahul.sharma25june@gmail.com>
2025-07-17 00:15:53 +0530

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const AboutUs = () => {
   const stats = [
-    { number: '500+', label: '“Trusted by hundreds of professionals who’ve found the right fit”. “We’ve helped 500+ professionals land roles that fit, grow, and inspire. Our approach goes beyond placement — it’s about long-term success.”' },
+    { number: '100+', label: '“Trusted by hundreds of professionals who’ve found the right fit”. “We’ve helped 500+ professionals land roles that fit, grow, and inspire. Our approach goes beyond placement — it’s about long-term success.”' },
     { number: '98%', label: '“Trusted to deliver — with one of the highest success           rates in the industry”with “Proven track record of successful placements and         client satisfaction”' },
     { number: '100%', label: 'Dedicated Account Managers for Every Client & Candidate with “Seamless Communication. Real-Time Updates.”' },
-    { number: '5+', label: '“Professionals with deep, real-world experience — 5+ years across core technologies”' },
+    { number: 'Tech Hiring', label: '“We work with engineers, architects, and product talent who’ve delivered in the real world — in code, in systems, and in growth. They're not just experienced — they’re execution-ready.”' },
   ];
 
   const team = [
